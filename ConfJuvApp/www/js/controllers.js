85963eadbf4a92916da1d3a20de661fb18046e02
Caio SBA <caiosba@gmail.com>
2015-05-14 15:57:51 -0300

    Adding configuration management and login modal

--- ConfJuvApp/www/js/controllers.js
+++ ConfJuvApp/www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('confjuvapp.controllers', [])
-  .controller('ProposalCtrl', function($scope) {
+  .controller('ProposalCtrl', function($scope, $ionicModal) {
+
     // FIXME: This list should come from the server
     $scope.proposalList = [
       {
@@ -9,4 +10,35 @@ angular.module('confjuvapp.controllers', [])
         title: 'Desmilitarizar a polícia'
       }
     ];
-  });
+
+    // Login modal
+
+    // Initiate the modal
+    $ionicModal.fromTemplateUrl('modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    // Function to open the modal
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+
+    // Function to close the modal
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
+    // Function to login
+    $scope.Login = function(data) {
+      $scope.closeModal();
+    };
+
+  }); // Ends controller
--- README.txt
+++ README.txt
@@ -14,3 +14,5 @@ This application is built on top of Ionic, so you need this framework.
 # `ionic emulate (android|ios)`
 
 In order to compile the theme, you need to run: `gulp sass`
+
+You need to configure the application by creating a file `www/js/config.js` based on `www/js/config.js.example`.
