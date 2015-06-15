3a41cab62fec901241ea8534ed1a3e66ba98a1c9
Caio SBA <caiosba@gmail.com>
2015-06-15 02:10:51 -0300

    Hide the close rightbar button on large screens

--- ConfJuvApp/www/js/app.js
+++ ConfJuvApp/www/js/app.js
@@ -16,3 +16,33 @@ angular.module('confjuvapp', ['ionic', 'confjuvapp.controllers', 'confjuvapp.fil
     }
   });
 })
+
+.directive('hideWhen', ['$window', function($window) {
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      function checkExpose() {
+        var mq = $attr.hideWhen == 'large' ? '(min-width:768px)' : $attr.hideWhen;
+        if ($window.matchMedia(mq).matches) {
+          $element.addClass('ng-hide');
+        }
+        else {
+          $element.removeClass('ng-hide');   
+        }
+      }
+      function onResize() {
+        debouncedCheck();
+      }
+      var debouncedCheck = ionic.debounce(function() {
+        $scope.$apply(function() {
+          checkExpose();
+        });
+      }, 300, false);
+      checkExpose();
+      ionic.on('resize', onResize, $window);
+      $scope.$on('$destroy', function(){
+        ionic.off('resize', onResize, $window);
+      });
+    }
+  };
+}]);
