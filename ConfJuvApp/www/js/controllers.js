395f5ad9a93423fba1be490c3954b16066b9282e
Caio SBA <caiosba@gmail.com>
2015-07-05 17:27:28 -0300

    Display proposal author on proposal list and single proposal view

--- ConfJuvApp/www/js/controllers.js
+++ ConfJuvApp/www/js/controllers.js
@@ -295,7 +295,7 @@ angular.module('confjuvapp.controllers', [])
 
     $scope.loadProposals = function(token, topic) {
       $scope.loading = true;
-      var params = '?private_token=' + token + '&fields=title,image,body,abstract,id,tag_list&content_type=ProposalsDiscussionPlugin::Proposal';
+      var params = '?private_token=' + token + '&fields=title,image,body,abstract,id,tag_list,created_by&content_type=ProposalsDiscussionPlugin::Proposal';
       var path = 'articles/' + topic.id + '/children' + params;
 
       $http.get(ConfJuvAppUtils.pathTo(path))
