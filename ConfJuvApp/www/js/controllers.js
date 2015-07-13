cb42f3f1af29ad4c8691c7feac114e74eafaa557
Caio SBA <caiosba@gmail.com>
2015-07-13 01:33:57 -0300

    Better disposition of tags and buttons on proposal screen

--- ConfJuvApp/www/js/controllers.js
+++ ConfJuvApp/www/js/controllers.js
@@ -364,7 +364,7 @@ angular.module('confjuvapp.controllers', [])
       }
       else {
         // Initiate the modal
-        $ionicModal.fromTemplateUrl('html/_proposal.html?2', {
+        $ionicModal.fromTemplateUrl('html/_proposal.html?3', {
           scope: $scope,
           animation: 'slide-in-up'
         }).then(function(modal) {
