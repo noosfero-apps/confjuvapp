c7e53dc638abc391fcb01c5d9fcf4a18fb21255d
Caio SBA <caiosba@gmail.com>
2015-07-22 14:14:04 -0300

    Updating script

--- ConfJuvApp/release-live.sh
+++ ConfJuvApp/release-live.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Generates APK ready for Play Store
 # Remember to increment the "version" value in config.xml when generating a new release
-#cordova plugin rm org.apache.cordova.console
-#cordova build --release android
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore confjuvapp-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk confjuvapp_key
+# More information at http://ionicframework.com/docs/guide/publishing.html
+cordova plugin rm org.apache.cordova.console
+cordova build --release android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore confjuvapp-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk confjuvapp_key
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk confjuvapp.apk
 mv confjuvapp.apk builds/confjuvapp-live.apk
 echo "Now upload builds/confjuvapp-live.apk to Play Store"
