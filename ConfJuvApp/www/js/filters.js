a5c8af32b873ae59c3edb0bcde46ff65c37ca370
Caio SBA <caiosba@gmail.com>
2015-08-01 15:41:36 -0300

    Filter tags

--- ConfJuvApp/www/js/filters.js
+++ ConfJuvApp/www/js/filters.js
@@ -1,16 +1,20 @@
 angular.module('confjuvapp.filters', []).
-  filter('htmlToPlainText', function() {
-    return function(text) {
-      return String(text).replace(/<[^>]+>/gm, '');
-    };
-  }
-).filter('categoryType', function() {
-    return function(categories, type) {
-      for (var i = 0; i < categories.length; i++) {
-        if(categories[i].type.toLowerCase() == type){
-          return categories[i].name;
-        }
+filter('htmlToPlainText', function() {
+  return function(text) {
+    return String(text).replace(/<[^>]+>/gm, '');
+  };
+}).
+filter('categoryType', function() {
+  return function(categories, type) {
+    for (var i = 0; i < categories.length; i++) {
+      if (categories[i].type.toLowerCase() == type) {
+        return categories[i].name;
       }
-    };
-  }
-);
+    }
+  };
+}).
+filter('tagFilter', function() {
+  return function(text) {
+    return String(text).replace(/#/gm, '');
+  };
+});
